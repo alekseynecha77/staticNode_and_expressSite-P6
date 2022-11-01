caadcca24f8de50c7b3f930b98d14eed4426d73b
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-01 19:46:20 -0400

    update

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const { projects } = require('./data/data.json');
-
 const app = express();
 
 app.use('/static' , express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render(`index`);
-    res.locals(data.projects);
+    res.render('index', { projects });
 });
 app.get('/about', (req, res) => {
     res.render(`about`);
 });
-app.get('/projects/:id', (req, res) => {
+app.get('/projects/:id', (req, res, next) => {
     const{ id } = req.params;
+    if(id <= projects.length && id >= 0){
+        res.render('project', { project: projects[id] });
+    
+    }else{
+        next();
+    }
 });
 
 app.listen(3000, () => {
