11259534a442734e5458e42570aeefc39b7f6ed3
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-03 20:10:37 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const { projects } = require('./data/data.json');
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/static' , express.static('public'));
 app.set('view engine', 'pug');
@@ -20,6 +25,17 @@ app.get('/projects/:id', (req, res, next) => {
         next();
     }
 });
+app.use((req, res, next)=>{
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+app.use((err, req, res, next)=>{
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000')
