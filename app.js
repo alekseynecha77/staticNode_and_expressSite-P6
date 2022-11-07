423571ee8979189ff904382dc7e67dc4244c1d37
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-07 13:15:19 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const { projects } = require('./data/data.json');
 const app = express();
 
 
@@ -10,35 +8,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use('/static' , express.static('public'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index', { projects });
-});
-app.get('/about', (req, res) => {
-    res.render(`about`);
-});
 
-app.get('/project/:id', (req, res, next) => {
-    const{ id } = req.params;
-    if(id <= projects.length && id >= 0){
-        res.render('project', { project: projects[id] });
-    
-    }else{
-        next();
-    }
-});
 app.use((req, res, next)=>{
     console.log('not found page try different ending');
     const err = new Error('Not found');
     err.status = 404;
-    next(err);
-
-});
-app.use((req, res, next)=>{
-    console.log('error');
-
-    const err = new Error();
-    err.message = `500 error thrown`;
-    err.status = 500;
     next(err);
 
 });
@@ -48,11 +22,11 @@ app.use((err, req, res, next)=>{
     if (err) {
         console.log('Global error handler called', err)
     } 
-    if(err.status(404) === 404){
+    if(err.status === 404){
     
     res.status(404).render('page-not-found', {err});
 }else{
-    err.message = err.message || `oops look like have error`
+    err.message = err.message || `oops it looks like you got into wrong page`
     res.status(err.status || 500).render('error', {err});
 }
 });
@@ -61,3 +35,5 @@ app.use((err, req, res, next)=>{
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000')
 });
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { projects } = require('../data/data.json');
+/* GET generated error route - create and throw 500 server error */
+
+
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+});
+app.get('/about', (req, res) => {
+    res.render(`about`);
+});
+
+app.get('/project/:id', (req, res, next) => {
+    const{ id } = req.params;
+    if(id <= projects.length && id >= 0){
+        res.render('project', { project: projects[id] });
+    
+    }else{
+        next();
+    }
+});
+router.get('/error', (req, res, next) => {
+
+    // Log out custom error handler indication
+    console.log('Custom error route called');
+    const err = new Error();
+    err.message = `Custom 500 error thrown`
+    err.status = 500;
+    throw err;
+});  
+module.exports = router;
